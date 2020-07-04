8fdfe3f158b90002aa00c11dbc7422b2085c2944
fcsouza <fcsouza18@hotmail.com>
2020-07-04 20:55:55 -0300

    include upload endpoint

--- backend/src/services/ImportTransactionsService.ts
+++ backend/src/services/ImportTransactionsService.ts
@@ -1,12 +1,11 @@
-import { getCustomRepository, getRepository, In } from 'typeorm';
-import csvParse from 'csv-parse';
+import { getCustomRepository } from 'typeorm';
 import fs from 'fs';
 
 import Product from '../models/Product';
 
 import ProductsRepository from '../repositories/ProductsRepository';
 
-interface CSVTransaction {
+interface JSONProduction {
   title: string;
   type: string;
   rating: number;
@@ -17,31 +16,11 @@ class ImportProductsService {
   async execute(filePath: string): Promise<Product[]> {
     const productRepository = getCustomRepository(ProductsRepository);
 
-    const productsReadStream = fs.createReadStream(filePath);
-
-    const parses = csvParse({
-      from_line: 2,
-    });
-
-    const parseCSV = productsReadStream.pipe(parses);
-
-    const products: CSVTransaction[] = [];
-
-    parseCSV.on('data', async line => {
-      const [title, type, rating, price] = line.map((cell: string) =>
-        cell.trim(),
-      );
-
-      if (!title || !type || !rating || !price) return;
-
-
-      products.push({ title, type, rating, price });
-    });
-
-    await new Promise(resolve => parseCSV.on('end', resolve));
+    const productsJson = fs.readFileSync(filePath, "utf-8");
+    const productsObjects: JSONProduction[] = JSON.parse(productsJson);
 
     const createdProducts = productRepository.create(
-      products.map(product => ({
+      productsObjects.map(product => ({
         title: product.title,
         type: product.type,
         rating: product.rating,
